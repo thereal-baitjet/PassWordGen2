d3064dfc39eb8091dacbb8733266e74970b86376
baitjet <baitjet@gmail.com>
2021-02-26 20:45:54 -0500

    made a prompt function concatonate string

--- 02-Homework2/Develop/script.js
+++ 02-Homework2/Develop/script.js
@@ -18,48 +18,49 @@ var lowerCaseList = "abcdefghijklmnopqrstuvwxyz";
 var numList = "1234567890";
 var specialList = "~!@#$%^&*()_+{}|:<>?[];\",'/";
 var totalTypes = "";
-var upperCaseConfirm = "";
-var lowerCaseConfirm = "";
-var numListConfirm = "";
-var typeIndex = "";
-var typeAdder = "";
-var finalPassword = "";
-var specialListConfirm = "";
+var upperCaseConfirm = false;
+var lowerCaseConfirm =  false;
+var numListConfirm = false;
+var result = "";
+var specialListConfirm = false;
+var userSize = 0;
 
-function generatePassword()  {
+function generatePassword(promptUser)  {
 
-function generate() {
+//this function prompts the user and sets the varialbles baased on user input
+
+function promptUser() {
 
 var userInput = prompt("Enter a value between 8 - 128");
-var userSize = parseInt(userInput);
-var numListConfirm = confirm("Would you like yonumbers?");
-var lowerCaseConfirm = confirm("would you like your password to contain lower case letters?");
-var upperCaseConfirm = confirm("would you like your password to contain Upper case letters?");
-var specialListConfirm = confirm("Would you like special characters?");
-
-  
+userSize = parseInt(userInput);
 if (userSize < 8 || userSize > 128 || isNaN(userSize)) {  
-alert("hey, hey, hey enter the right value! I repeat from 8 - 128")
-return;
+  alert("hey, hey, hey enter the right value! I repeat from 8 - 128")
+  promptUser()
 
 }
+numListConfirm = confirm("Would you like numbers?");
+lowerCaseConfirm = confirm("would you like your password to contain lower case letters?");
+upperCaseConfirm = confirm("would you like your password to contain Upper case letters?");
+specialListConfirm = confirm("Would you like special characters?");
+return
+
+} 
 
 if (numListConfirm) {
-  totalTypes = numList + totalTypes;
-}
+totalTypes = numList + totalTypes;
+ }
 
 if (lowerCaseConfirm) {
-  totalTypes  = lowerlCaselist + totalTypes;
+totalTypes  = lowerlCaselist + totalTypes;
 }
 
 if (upperCaseConfirm) {
-  totalTypes = upperCaseList + totalTypes;
+totalTypes = upperCaseList + totalTypes;
 }
 
 if (specialListConfirm) {
-  totalTypes = specialList + totalTypes;
+totalTypes = specialList + totalTypes;
 
 }
-
-typeAdder = totalTypes.lenght
-
+totalTypes
+}
